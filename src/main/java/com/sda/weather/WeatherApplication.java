5c712f5ec9af1236c612ee4dc96af7f4853b2543
KrzysztofDolanski <k.dolanski@wp.pl>
2020-12-07 19:14:23 +0100

    Working in thymeleaf

--- src/main/java/com/sda/weather/WeatherApplication.java
+++ src/main/java/com/sda/weather/WeatherApplication.java
@@ -2,6 +2,7 @@ package com.sda.weather;
 
 import com.sda.weather.forecast.OpenWeatherProperties;
 import com.sda.weather.localisation.WeatherStackProperties;
+import com.sda.weather.security.MvcConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
---
+++ src/main/java/com/sda/weather/peoples/People.java
@@ -0,0 +1,27 @@
+package com.sda.weather.peoples;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Component
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class People {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    private String roles;
+}
